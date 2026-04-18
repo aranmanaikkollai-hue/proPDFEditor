e1645e9b248d2e5045e3cd2180ea40c325938be3
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-04-18 12:39:15 +0530

    Delete MainActivity.kt

--- MainActivity.kt
+++
@@ -1,9 +0,0 @@
-// New imports
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.pdf.PdfRenderer
-import android.os.ParcelFileDescriptor
-import java.io.File
-import java.io.FileOutputStream
-
-// Existing code ...
