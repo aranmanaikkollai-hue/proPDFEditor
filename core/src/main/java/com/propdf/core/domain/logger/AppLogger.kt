ac49598e2c45aa3630e705f918a683f4d869c6e7
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-26 20:07:10 +0530

    Add files via upload

---
+++ core/src/main/java/com/propdf/core/domain/logger/AppLogger.kt
@@ -0,0 +1,11 @@
+package com.propdf.core.domain.logger
+
+/**
+ * Simple logging abstraction for domain layer.
+ */
+interface AppLogger {
+    fun d(tag: String, message: String)
+    fun i(tag: String, message: String)
+    fun w(tag: String, message: String, throwable: Throwable? = null)
+    fun e(tag: String, message: String, throwable: Throwable? = null)
+}
