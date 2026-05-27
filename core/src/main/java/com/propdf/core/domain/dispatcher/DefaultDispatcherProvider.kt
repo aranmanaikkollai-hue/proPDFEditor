0c0b1f7f66b871d689d14d8573b072c2df585a57
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-27 14:31:18 +0530

    Add files via upload

---
+++ core/src/main/java/com/propdf/core/domain/dispatcher/DefaultDispatcherProvider.kt
@@ -0,0 +1,12 @@
+package com.propdf.core.domain.dispatcher
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+class DefaultDispatcherProvider @Inject constructor() : DispatcherProvider {
+    override val io: CoroutineDispatcher = Dispatchers.IO
+    override val default: CoroutineDispatcher = Dispatchers.Default
+    override val main: CoroutineDispatcher = Dispatchers.Main
+    override val unconfined: CoroutineDispatcher = Dispatchers.Unconfined
+}
---
+++ core/src/main/java/com/propdf/core/domain/dispatcher/DispatcherProvider.kt
@@ -0,0 +1,10 @@
+package com.propdf.core.domain.dispatcher
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val main: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+}
