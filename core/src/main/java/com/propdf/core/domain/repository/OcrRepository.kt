92e3fb433adf0254a9a939800a3a104d9bdd27dc
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-05-27 15:06:24 +0530

    Delete core/src/main/java/com/propdf/core/domain/repository/OcrRepository.kt

--- core/src/main/java/com/propdf/core/domain/repository/OcrRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.propdf.core.domain.repository
-
-import android.graphics.Bitmap
-import com.propdf.core.domain.result.AppResult
-
-interface OcrRepository {
-    suspend fun recognizeText(bitmap: Bitmap, language: String = "eng"): AppResult<String>
-    suspend fun getSupportedLanguages(): AppResult<List<String>>
-}
