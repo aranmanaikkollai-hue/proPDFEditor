d00fe9765bd95fe46c1c786e836427bd62009e07
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-06-05 10:28:11 +0530

    Add files via upload

---
+++ viewer/src/main/java/com/propdf/viewer/annotation/render/UnifiedAnnotationRenderer.kt
@@ -0,0 +1,21 @@
+package com.propdf.viewer.annotation.render
+
+import android.graphics.Canvas
+import com.propdf.viewer.annotation.manager.AnnotationManager
+
+class UnifiedAnnotationRenderer {
+
+    private val renderer = AnnotationRenderer()
+
+    fun render(
+        canvas: Canvas,
+        annotationManager: AnnotationManager,
+        pageIndex: Int,
+        pageWidth: Float,
+        pageHeight: Float
+    ) {
+        val annotations = annotationManager.getAnnotationsForPage(pageIndex)
+        val selectedId = annotationManager.selectedAnnotationId
+        renderer.render(canvas, annotations, pageWidth, pageHeight, selectedId)
+    }
+}
