5fde07434b293c6877420da956005a0284224610
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-06-05 10:18:48 +0530

    Add files via upload

---
+++ viewer/src/main/java/com/propdf/viewer/model/ViewerMode.kt
@@ -0,0 +1,5 @@
+package com.propdf.viewer.model
+
+// All types previously defined here have been moved to their own files:
+//   ViewMode.kt, ViewerTheme.kt, ZoomMode.kt, PdfTab.kt,
+//   PageThumbnail.kt, ScrollDirection.kt, RenderPriority.kt
