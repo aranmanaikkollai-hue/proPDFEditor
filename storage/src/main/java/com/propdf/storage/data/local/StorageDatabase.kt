e2462b6f55191814aaf00610347670387e4f01ee
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-06-22 19:05:21 +0530

    Create StorageDatabase class for Room database

---
+++ storage/src/main/java/com/propdf/storage/data/local/StorageDatabase.kt
@@ -0,0 +1,31 @@
+package com.propdf.storage.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.propdf.storage.data.local.entity.PersistedUriEntity
+
+@Database(
+    entities = [PersistedUriEntity::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class StorageDatabase : RoomDatabase() {
+    abstract fun persistedUriDao(): PersistedUriDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: StorageDatabase? = null
+
+        fun getInstance(context: Context): StorageDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    StorageDatabase::class.java,
+                    "storage.db"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+}
