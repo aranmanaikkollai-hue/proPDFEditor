a3e87ba23c3a634bab7f50935cc7b33217a5c669
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-06-28 22:16:54 +0530

    Add files via upload

---
+++ app/src/main/java/com/propdf/editor/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.propdf.editor.di
+
+// SafEngine is provided by :core's CoreModule.
+// No duplicate @Provides here.
---
+++ app/src/main/java/com/propdf/editor/di/WorkManagerModule.kt
@@ -0,0 +1,4 @@
+package com.propdf.editor.di
+
+// WorkManager is provided by :editor's WorkManagerModule.
+// No duplicate @Provides here.
