8cd92d9207d7a6d86e2ecf294fc57fd244fbf557
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-10 20:08:08 +0530

    Update ProPDFApp.kt

--- app/src/main/java/com/propdf/editor/ProPDFApp.kt
+++ app/src/main/java/com/propdf/editor/ProPDFApp.kt
@@ -1,36 +1,16 @@
 package com.propdf.editor
 
 import android.app.Application
-import android.os.StrictMode
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
+import com.propdf.editor.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
+import timber.log.Timber
 
 @HiltAndroidApp
-class ProPDFApplication : Application(), Configuration.Provider {
-
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .setMinimumLoggingLevel(android.util.Log.INFO)
-            .build()
-
+class ProPDFApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        
         if (BuildConfig.DEBUG) {
-            StrictMode.setThreadPolicy(
-                StrictMode.ThreadPolicy.Builder()
-                    .detectDiskReads()
-                    .detectDiskWrites()
-                    .detectNetwork()
-                    .penaltyLog()
-                    .build()
-            )
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
