edee4f829d7b25a0300e2b49a33ee8a4238beb4a
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-11 16:54:57 +0530

    Remove debug logging from ProPDFApp
    Remove Timber logging setup for debug builds.

--- app/src/main/java/com/propdf/editor/ProPDFApp.kt
+++ app/src/main/java/com/propdf/editor/ProPDFApp.kt
@@ -1,16 +1,11 @@
 package com.propdf.editor
 
 import android.app.Application
-import com.propdf.editor.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
 class ProPDFApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
     }
 }
