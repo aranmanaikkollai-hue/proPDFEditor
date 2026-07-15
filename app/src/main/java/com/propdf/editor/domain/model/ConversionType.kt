5c0bba087e5b4089bf5797b91522a71314016005
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-15 10:51:03 +0530

    Add ConversionType enum for file conversions

---
+++ app/src/main/java/com/propdf/editor/domain/model/ConversionType.kt
@@ -0,0 +1,13 @@
+package com.propdf.editor.domain.model
+
+enum class ConversionType {
+    PDF_TO_IMAGES,
+    IMAGES_TO_PDF,
+    PDF_TO_TXT,
+    TXT_TO_PDF,
+    HTML_TO_PDF,
+    MARKDOWN_TO_PDF,
+    MERGE_IMAGES,
+    SPLIT_IMAGE,
+    ZIP_EXPORT
+}
