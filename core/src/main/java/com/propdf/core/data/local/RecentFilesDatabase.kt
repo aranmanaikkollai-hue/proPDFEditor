b545e94b3061005960dff44e2cfcc05d4fda943f
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-17 17:22:59 +0530

    Refactor RecentFilesDatabase to ProPDFDatabase
    Updated database version and added CompressionHistoryEntity. Introduced type converters for date handling.

--- core/src/main/java/com/propdf/core/data/local/RecentFilesDatabase.kt
+++ core/src/main/java/com/propdf/core/data/local/RecentFilesDatabase.kt
@@ -2,12 +2,30 @@ package com.propdf.core.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
-    entities = [RecentFileEntity::class],
-    version = 1,
-    exportSchema = false
+    entities = [
+        RecentFileEntity::class,
+        CompressionHistoryEntity::class
+    ],
+    version = 2,
+    exportSchema = true
 )
-abstract class RecentFilesDatabase : RoomDatabase() {
-    abstract fun recentFilesDao(): RecentFilesDao
+@TypeConverters(CompressionConverters::class, DateConverters::class)
+abstract class ProPDFDatabase : RoomDatabase() {
+    abstract fun recentFileDao(): RecentFileDao
+    abstract fun compressionHistoryDao(): CompressionHistoryDao
+}
+
+class DateConverters {
+    @androidx.room.TypeConverter
+    fun fromTimestamp(value: Long?): java.util.Date? {
+        return value?.let { java.util.Date(it) }
+    }
+
+    @androidx.room.TypeConverter
+    fun dateToTimestamp(date: java.util.Date?): Long? {
+        return date?.time
+    }
 }
