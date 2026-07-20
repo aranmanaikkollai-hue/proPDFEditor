49b02687a8740ca85787208157e232653a259054
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-20 11:56:22 +0530

    Add BatchJobType enum for batch job operations

---
+++ app/src/main/java/com/propdfeditor/batch/data/entity/BatchJobType.kt
@@ -0,0 +1,15 @@
+package com.propdfeditor.batch.data.util
+
+enum class BatchJobType {
+    MERGE,
+    SPLIT,
+    RENAME,
+    WATERMARK,
+    ROTATE,
+    COMPRESS,
+    OCR,
+    ENCRYPT,
+    DECRYPT,
+    EXPORT,
+    DELETE
+}
