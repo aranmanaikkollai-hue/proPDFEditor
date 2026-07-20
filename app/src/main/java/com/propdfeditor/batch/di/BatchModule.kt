e19423fee91938d110e59ed952150038ded69f70
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-20 17:01:39 +0530

    Remove unused Dagger provider methods from BatchModule
    Removed several provider methods for BatchJobRepository, BatchNotificationManager, PdfProcessor, BatchWorkScheduler, and BatchWorkerFactory.

--- app/src/main/java/com/propdfeditor/batch/di/BatchModule.kt
+++ app/src/main/java/com/propdfeditor/batch/di/BatchModule.kt
@@ -2,11 +2,6 @@ package com.propdfeditor.batch.di
 
 import android.content.Context
 import com.propdfeditor.batch.data.database.BatchDatabase
-import com.propdfeditor.batch.repository.BatchJobRepository
-import com.propdfeditor.batch.scheduler.BatchWorkScheduler
-import com.propdfeditor.batch.util.BatchNotificationManager
-import com.propdfeditor.batch.util.PdfProcessor
-import com.propdfeditor.batch.worker.BatchWorkerFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -22,47 +17,5 @@ object BatchModule {
     @Singleton
     fun provideBatchDatabase(@ApplicationContext context: Context): BatchDatabase {
         return BatchDatabase.getInstance(context)
-    }
-
-    @Provides
-    @Singleton
-    fun provideBatchJobRepository(
-        @ApplicationContext context: Context,
-        database: BatchDatabase
-    ): BatchJobRepository {
-        return BatchJobRepository(context, database)
-    }
-
-    @Provides
-    @Singleton
-    fun provideBatchNotificationManager(
-        @ApplicationContext context: Context
-    ): BatchNotificationManager {
-        return BatchNotificationManager(context)
-    }
-
-    @Provides
-    @Singleton
-    fun providePdfProcessor(): PdfProcessor {
-        return PdfProcessor()
-    }
-
-    @Provides
-    @Singleton
-    fun provideBatchWorkScheduler(
-        @ApplicationContext context: Context,
-        repository: BatchJobRepository
-    ): BatchWorkScheduler {
-        return BatchWorkScheduler(context, repository)
-    }
-
-    @Provides
-    @Singleton
-    fun provideBatchWorkerFactory(
-        repository: BatchJobRepository,
-        notificationManager: BatchNotificationManager,
-        pdfProcessor: PdfProcessor
-    ): BatchWorkerFactory {
-        return BatchWorkerFactory(repository, notificationManager, pdfProcessor)
     }
 }
