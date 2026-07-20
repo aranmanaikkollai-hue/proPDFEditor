9df8c07bf5c8fb90f3008ef11225c6531509a768
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-20 15:13:19 +0530

    Add TypeConverter methods for Uri conversion

--- app/src/main/java/com/propdfeditor/batch/data/util/UriListConverter.kt
+++ app/src/main/java/com/propdfeditor/batch/data/util/UriListConverter.kt
@@ -14,4 +14,15 @@ class UriListConverter {
         if (data.isEmpty()) return emptyList()
         return data.split(",").map { Uri.parse(it) }
     }
+
+    @TypeConverter
+    fun fromUri(uri: Uri?): String? {
+        return uri?.toString()
+    }
+
+    @TypeConverter
+    fun toUri(data: String?): Uri? {
+        if (data.isNullOrEmpty()) return null
+        return Uri.parse(data)
+    }
 }
