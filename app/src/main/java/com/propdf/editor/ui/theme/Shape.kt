5771e5300535c63e587fe875132d7d65906322aa
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-24 12:16:30 +0530

    Change extraLarge corner size from 24dp to 28dp

--- app/src/main/java/com/propdf/editor/ui/theme/Shape.kt
+++ app/src/main/java/com/propdf/editor/ui/theme/Shape.kt
@@ -9,6 +9,5 @@ val Shapes = Shapes(
     small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(12.dp),
     large = RoundedCornerShape(16.dp),
-    extraLarge = RoundedCornerShape(24.dp)
+    extraLarge = RoundedCornerShape(28.dp)
 )
-
