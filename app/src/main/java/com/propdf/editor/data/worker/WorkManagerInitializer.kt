9463d440e6c38f8251d1284bba33ccac81a5551c
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-30 06:43:10 +0530

    Add files via upload

---
+++ app/src/main/java/com/propdf/editor/data/worker/WorkManagerInitializer.kt
@@ -0,0 +1,16 @@
+package com.propdf.editor.data.worker
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.propdf.editor.data.index.DocumentIndexWorker
+
+class WorkManagerInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        DocumentIndexWorker.schedule(context)
+        DuplicateScanWorker.schedulePeriodic(context)
+        SmartFolderRefreshWorker.schedule(context)
+        DocumentTableBackfillWorker.scheduleOnce(context)
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+}
