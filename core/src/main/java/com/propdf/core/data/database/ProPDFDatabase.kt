7941419d0a4857b03612d899be63dcbf44c13a04
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-07-30 15:33:34 +0530

    Add files via upload

---
+++ core/src/main/java/com/propdf/core/data/database/ProPDFDatabase.kt
@@ -0,0 +1,30 @@
+package com.propdf.core.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.propdf.core.data.entity.*
+import com.propdf.core.data.local.dao.*
+
+@Database(
+    entities = [
+        PdfDocumentEntity::class,
+        DocumentTagEntity::class,
+        DocumentTagCrossRef::class,
+        DocumentCollectionEntity::class,
+        RecentActivityEntity::class,
+        FormFieldEntity::class,
+        FormDataEntity::class
+    ],
+    version = 3,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
+abstract class ProPDFDatabase : RoomDatabase() {
+    abstract fun pdfDocumentDao(): PdfDocumentDao
+    abstract fun documentTagDao(): DocumentTagDao
+    abstract fun documentCollectionDao(): DocumentCollectionDao
+    abstract fun recentActivityDao(): RecentActivityDao
+    abstract fun formFieldDao(): FormFieldDao
+    abstract fun formDataDao(): FormDataDao
+}
