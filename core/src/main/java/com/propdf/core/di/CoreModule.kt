6526b932f2b5ee41d2dbeb517700e52756f82012
aranmanaikkollai-hue <aranmanaikkollai@gmail.com>
2026-08-02 21:57:25 +0530

    Add import for File in CoreModule.kt

--- core/src/main/java/com/propdf/core/di/CoreModule.kt
+++ core/src/main/java/com/propdf/core/di/CoreModule.kt
@@ -12,6 +12,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
+import java.io.File
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
